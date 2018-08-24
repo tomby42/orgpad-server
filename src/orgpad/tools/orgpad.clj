3ac3328dc33bf74f811bb8ea61d4e5cb510130db
Tomas tomby Bily <tomby@ucw.cz>
2018-08-24 17:55:11 +0200

    atom unit-view moved to global db and resend

--- src/orgpad/tools/orgpad.clj
+++ src/orgpad/tools/orgpad.clj
@@ -1,6 +1,7 @@
 (ns orgpad.tools.orgpad
   (:require
-   [datascript.core :as d]))
+   [datascript.core :as d]
+   [clojure.set :as set]))
 
 ;; Query determining local data in db
 (def local-qry '[:find [(pull ?v [*]) ...]
@@ -9,6 +10,7 @@
                  [?v :orgpad/type :orgpad/unit-view]])
 
 ;; TODO - move props-refs pointing to local entities to local db and remove them from global db
+;;        atoms move to global db
 (defn split-global-local-db
   "Returns [global local] part of db. Global part is common to all clients. Local
   is just for single client."
@@ -42,12 +44,20 @@
 (defn split-global-local-update
   [ldb update-stream]
   (let [update-db (-> (d/empty-db {}) (d/with update-stream) :db-after)
-        local-ids (->> update-db
+        all-local-ids (->> update-db
+                           (d/q '[:find [?v ...]
+                                  :in $
+                                  :where
+                                  [?v :orgpad/type :orgpad/unit-view]])
+                           set)
+        atom-ids (->> update-db
                        (d/q '[:find [?v ...]
                               :in $
                               :where
-                              [?v :orgpad/type :orgpad/unit-view]])
-                       set)]
+                              [?v :orgpad/atom]])
+                       set)
+        local-ids (set/difference all-local-ids atom-ids)]
+
     (reduce (fn [[gupdate lupdate] datom]
               (if (or (is-local? ldb local-ids (.-e datom))
                       (and (= (.-a datom) :orgpad/props-refs)
@@ -60,6 +70,10 @@
 (require '[datascript.core :as d])
 (require '[datascript.db :as db])
 (require '[orgpad.tools.orgpad :as ot])
-(def s [(db/datom 1 :orgpad/type :orgpad/unit) (db/datom 2 :orgpad/type :orgpad/unit-view)])
+(def s [(db/datom 1 :orgpad/type :orgpad/unit) (db/datom 1 :orgpad/props-refs 2) (db/datom 1 :orgpad/props-refs 3)
+        (db/datom 2 :orgpad/type :orgpad/unit-view)
+        (db/datom 3 :orgpad/type :orgpad/unit-view) (db/datom 3 :orgpad/atom "a")])
 (ot/split-global-local-update (d/empty-db {}) s)
+
+(def d1 (-> (d/empty-db {}) (d/with s) :db-after))
 )
